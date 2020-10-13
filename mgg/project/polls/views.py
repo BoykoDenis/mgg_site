6ae8b7d77743ce3a2affe2152c4b18880fa1cb69
Denys <denya.boyko@gmail.com>
2020-10-13 13:32:23 +0200

    first commit

---
+++ mgg/project/polls/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def index(request):
+    return HttpResponse("Hello world. You r in the polls index.")
+# Create your views here.
+
+def detail(request, question_id):
+    return HttpResponse("you r looking at question %s." % question_id)
+
+def results(request, question_id):
+    response = "U r looking ath the results of question %s."
+    return HttpResponse(response % question_id)
+
+def vote(request, question_id):
+    return HttpResponse("U r votin on question %s." % question_id)
