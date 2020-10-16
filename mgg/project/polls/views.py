b3a6f846997a023d6cee9cb590cbcf62678c1619
Denys <denya.boyko@gmail.com>
2020-10-16 16:24:08 +0200

    a few models added

--- mgg/project/polls/views.py
+++ mgg/project/polls/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Question
 
 def index(request):
-    return HttpResponse("Hello world. You r in the polls index.")
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    output = ', '.join([q.question_text for q in latest_question_list])
+    return HttpResponse(output)
 # Create your views here.
 
 def detail(request, question_id):
